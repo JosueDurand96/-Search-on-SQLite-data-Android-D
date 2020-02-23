dc18c0badc3f3488b068eac5d444475740a6d814
Josue <josue.durand8@gmail.com>
2020-02-23 18:44:51 -0500

    model

---
+++ app/src/main/java/com/josue/searchsqliteprueba/Model/Friend.java
@@ -0,0 +1,57 @@
+package com.josue.searchsqliteprueba.Model;
+
+public class Friend {
+    public int id;
+    public String name, address, email, phone;
+
+    public Friend(int id, String name, String address, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Friend() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
